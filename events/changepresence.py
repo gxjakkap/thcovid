4fc693b89fb086ffc79b26a67530341dcfd7925c
Jakkaphat <gunt@guntxjakka.me>
2021-12-25 07:53:00 +0700

    update events/changepresence.py
    - change data pulling to the correct data
    
    whoops lmao

--- events/changepresence.py
+++ events/changepresence.py
@@ -12,5 +12,5 @@ class ChangePresence(BaseEvent):
     async def run(self, client):
         r = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all')
         ans = r.json()
-        nc = ans[0]['total_case']
+        nc = ans[0]['new_case']
         await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name=f'{nc:,} new Covid cases'))
