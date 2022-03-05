9a61b8a890ce73531d24a4ae01484c1c30efc5af
Jakkaphat <gunt@guntxjakka.me>
2022-03-05 09:11:02 +0700

    fix TypeError un commands

--- commands/getpr.py
+++ commands/getpr.py
@@ -1,7 +1,8 @@
 import requests
 import nextcord
-from commands.base_command  import BaseCommand
+from commands.base_command import BaseCommand
 from utils import checkprovincevalidity
+
 
 class gbp(BaseCommand):
 
@@ -11,13 +12,12 @@ class gbp(BaseCommand):
         params = ["province"]
         super().__init__(description, params)
 
-    
     async def handle(self, params, message, client):
         url = 'https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces'
         r = requests.get(url)
         r.encoding = 'utf-8'
         ans = r.json()
-        if len(params)>1:
+        if len(params) > 1:
             usrinput = ' '.join(params)
         else:
             usrinput = params[0]
@@ -27,23 +27,31 @@ class gbp(BaseCommand):
             exist = False
         else:
             i = 0
-            while i<77:
-                if ans[i]['province']==pr[1]:
+            while i < 77:
+                if ans[i]['province'] == pr[1]:
                     exist = True
                     break
                 else:
-                    i+=1
+                    i += 1
         if exist:
             updated_text = f"{ans[i]['update_date']} GMT+7"
-            embed=nextcord.Embed(title="สถานการณ์ COVID-19 จังหวัด"+ans[i]['province'])
-            embed.add_field(name="🚑เคสใหม่", value=f"{ans[i]['new_case']:,}", inline=False)
-            embed.add_field(name="🚑เคสใหม่ (ยกเว้นเดินทางจากต่างประเทศ)", value=f"{ans[i]['new_case_excludeabroad']:,}", inline=False)
-            embed.add_field(name="🏥เคสสะสม", value=f"{ans[i]['total_case']:,}", inline=False)
-            embed.add_field(name="🏥เคสสะสม (ยกเว้นเดินทางจากต่างประเทศ)", value=f"{ans[i]['total_case_excludeabroad']:,}", inline=False)
-            embed.add_field(name="🪦เสียชีวิต", value=f"{ans[i]['new_death']:,}", inline=False)
-            embed.add_field(name="🪦เสียชีวิตสะสม", value=f"{ans[i]['total_death']:,}", inline=False)
-            embed.add_field(name="📅ข้อมูลอัปเดตล่าสุด", value=updated_text, inline=False)
+            embed = nextcord.Embed(
+                title="สถานการณ์ COVID-19 จังหวัด"+ans[i]['province'])
+            embed.add_field(name="🚑เคสใหม่",
+                            value=f"{ans[i]['new_case']:,}", inline=False)
+            embed.add_field(name="🚑เคสใหม่ (ยกเว้นเดินทางจากต่างประเทศ)",
+                            value=f"{ans[i]['new_case_excludeabroad']:,}", inline=False)
+            embed.add_field(name="🏥เคสสะสม",
+                            value=f"{ans[i]['total_case']:,}", inline=False)
+            embed.add_field(name="🏥เคสสะสม (ยกเว้นเดินทางจากต่างประเทศ)",
+                            value=f"{ans[i]['total_case_excludeabroad']:,}", inline=False)
+            embed.add_field(name="🪦เสียชีวิต",
+                            value=f"{ans[i]['new_death']:,}", inline=False)
+            embed.add_field(name="🪦เสียชีวิตสะสม",
+                            value=f"{ans[i]['total_death']:,}", inline=False)
+            embed.add_field(name="📅ข้อมูลอัปเดตล่าสุด",
+                            value=updated_text, inline=False)
             embed.set_footer(text="ข้อมูลจาก covid19.ddc.moph.go.th")
-            #await asyncio.gather(message.author.mention, message.channel.send(embed=embed))
-            await message.author.mention
+            # await asyncio.gather(message.author.mention, message.channel.send(embed=embed))
+            await message.channel.send(message.author.mention)
             await message.channel.send(embed=embed)
